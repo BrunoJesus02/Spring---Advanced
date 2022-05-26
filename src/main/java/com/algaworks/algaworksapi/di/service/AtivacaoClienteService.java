d35b1423fcf0da248fc4086aa151971553be08e9
BrunoJesus02 <rm87006@fiap.com.br>
2022-05-25 23:26:04 -0300

    Gerenciando Beans ambiguo, inicio da configuracao do properties e gerenciamento de prioridades dos Beans

--- src/main/java/com/algaworks/algaworksapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algaworksapi/di/service/AtivacaoClienteService.java
@@ -1,24 +1,23 @@
 package com.algaworks.algaworksapi.di.service;
 
 import com.algaworks.algaworksapi.di.modelo.Cliente;
+import com.algaworks.algaworksapi.di.notificacao.NivelUrgencia;
 import com.algaworks.algaworksapi.di.notificacao.Notificador;
-import com.algaworks.algaworksapi.di.notificacao.NotificadorEmail;
+import com.algaworks.algaworksapi.di.notificacao.TipoDoNotificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+    @Autowired()
     private Notificador notificador;
-
-    public AtivacaoClienteService(NotificadorEmail notificador) {
-        this.notificador = notificador;
-
-        System.out.println("AtivacaoClienteService " + notificador);
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+
 }
