8dc4cfea2a98c3076cb12a64330ab91c2208553c
BrunoJesus02 <rm87006@fiap.com.br>
2022-06-20 22:28:41 -0300

    Inicio do mapeamento relacional das tabelas

--- src/main/java/com/algaworks/algaworksapi/di/notificacao/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algaworksapi.di.notificacao;
-
-public enum NivelUrgencia {
-
-    URGENTE,
-    SEM_URGENCIA
-}
--- src/main/java/com/algaworks/algaworksapi/di/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algaworksapi.di.notificacao;
-
-import com.algaworks.algaworksapi.di.modelo.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
