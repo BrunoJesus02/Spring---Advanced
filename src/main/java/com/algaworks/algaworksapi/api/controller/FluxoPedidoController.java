facc303b906d9389c7c781ea8354ac1222e8695b
Bruno Jesus <brunobj222@gmail.com>
2023-10-05 22:55:31 -0300

    Implementando envio de email com spring

--- src/main/java/com/algaworks/algaworksapi/api/controller/FluxoPedidoController.java
+++ src/main/java/com/algaworks/algaworksapi/api/controller/FluxoPedidoController.java
@@ -3,22 +3,11 @@ package com.algaworks.algaworksapi.api.controller;
 import com.algaworks.algaworksapi.api.converter.input.PedidoModelInputConverter;
 import com.algaworks.algaworksapi.api.converter.input.PedidoResumoModelInputConverter;
 import com.algaworks.algaworksapi.api.converter.output.PedidoModelOutputConverter;
-import com.algaworks.algaworksapi.api.model.input.PedidoInput;
-import com.algaworks.algaworksapi.api.model.output.PedidoModel;
-import com.algaworks.algaworksapi.api.model.output.PedidoResumoModel;
-import com.algaworks.algaworksapi.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algaworksapi.domain.exception.NegocioException;
-import com.algaworks.algaworksapi.domain.model.Pedido;
-import com.algaworks.algaworksapi.domain.model.Usuario;
 import com.algaworks.algaworksapi.domain.repository.PedidoRepository;
-import com.algaworks.algaworksapi.domain.service.EmissaoPedidoService;
 import com.algaworks.algaworksapi.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pedidos/{codigoPedido}")
--- src/main/java/com/algaworks/algaworksapi/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algaworksapi/domain/service/FluxoPedidoService.java
@@ -5,16 +5,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.algaworks.algaworksapi.domain.service.EnvioEmailService.*;
+
 @Service
 public class FluxoPedidoService {
 
     @Autowired
     private EmissaoPedidoService pedidoService;
 
+    @Autowired
+    private EnvioEmailService envioEmailService;
+
     @Transactional
     public void confirmar(String codigoPedido) {
         Pedido pedido = pedidoService.buscarOuFalhar(codigoPedido);
         pedido.confirmar();
+
+        Mensagem mensagem = Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("O pedido de código <strong>" + pedido.getCodigo() + "<strong> foi confirmado!")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmailService.enviar(mensagem);
     }
 
     @Transactional
